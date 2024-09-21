0542bda61d9b7947fe0c4c0f61700df0102bd18e
BuuLe-cyper <142316601+BuuLe-cyper@users.noreply.github.com>
2024-09-22 00:06:11 +0700

    first commit

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    public Vector3 moveInput;
+    private void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
+    }
+}
